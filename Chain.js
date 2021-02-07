a9b7a1227d149b67f99565ce6273af2edaf35d52
muskan-ias <73595142+muskan-ias@users.noreply.github.com>
2021-02-07 12:36:18 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,44 @@
+class chain {
+constructor (bodyA,pointB) {
+var options = {
+bodyA: bodyA,
+pointB: pointB,
+stiffness: 0.04,
+length: 20
+
+
+
+}
+this.string = Constraint.create(options);
+this.pointB = pointB;
+World.add(world,this.string);
+
+}
+fly() {
+this.string.bodyA = null;
+
+
+}
+
+
+display(){
+if(this.string.bodyA){
+    var pointA = this.string.bodyA.position;
+    var pointB = this.pointB;
+    strokeWeight(5);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+    
+    
+
+}
+
+
+    
+}
+
+
+
+
+
+
+}
